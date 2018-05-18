f0c1e50743a2d3f38f93a07497b2a81475a36419
housirzero <housirzero@gmail.com>
2018-05-19 01:06:46 +0800

    write helloworld

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  CDemo
+//
+//  Created by housir on 2018/5/18.
+//  Copyright © 2018年 housir. All rights reserved.
+//
+
+#include <iostream>
+#include "Array.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
