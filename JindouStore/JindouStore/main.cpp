fe9201bcbd3470cfff6f3f547295cb875f71a460
housir <housir@housirdemacbook-pro.local>
2018-05-19 00:19:20 +0800

    hello world

---
+++ JindouStore/JindouStore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JindouStore
+//
+//  Created by housir on 2018/5/19.
+//  Copyright © 2018年 housir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
