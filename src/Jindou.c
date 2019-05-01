d1b8cbcfd634b81bca1d0c39b6adad2a499dfa46
koofun <1215198685@qq.com>
2019-05-01 12:36:01 +0800

    begin

---
+++ src/Jindou.c
@@ -0,0 +1,18 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *  JindouStore - memory cache daemon
+ *
+ *  Copyright 2019, All rights reserved.
+ *
+ *  Use and distribution licensed under the BSD license.  See
+ *  the LICENSE file for full text.
+ *
+ *  Authors:
+ *  WeAreOneTeam <https://github.com/WeAreOneTeam>
+ */
+
+#include "Jindou.h"
+int main(int argc, char **argv)
+{
+   return 0;
+}
